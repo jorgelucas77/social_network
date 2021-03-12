b6e49f48c19ba54ceed0d0f99a93231087ba078c
Jorge Lucas <jorgelucas77@gmail.com>
2021-03-12 11:22:08 +0200

    Create de posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: 'users/registrations'}
   devise_for :admins, skip: [:registrations], controllers: {sessions: 'admins/sessions'}
 
   namespace :admin do
     root to: "home#index"
     resources :admins 
+    resources :users, only:  :index
+    resources :posts, only: [:index, :show]
   end
 
   root to: "user/timeline#index"
 
   namespace :user do
-
+    get 'profile', to: "profile#show"
+    resources :posts, only: :create
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
