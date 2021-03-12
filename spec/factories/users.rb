78711867c451e001fe85b9767200b50e0243e91c
Jorge Lucas <jorgelucas77@gmail.com>
2021-03-12 12:32:45 +0200

    followers and following

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
     factory :user do
         name { 'Jorge' } 
-        email { 'user1@user.com' }
+        email { "user#{rand(1..1000)}@user.com" }
         phone { '999999999' }
         avatar { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/test-avatar.png'), 'image/png') }
         password { '1234567' }
